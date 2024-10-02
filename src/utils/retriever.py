ccbaf438c28d9c62af7e6c191b8881eb0ce4380d
luthfiraditya <luthfirdty@gmail.com>
2024-10-02 10:05:57 +0700

    feat: :sparkles: use timestamp metadata for filtering trend type query

--- src/utils/retriever.py
+++ src/utils/retriever.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 
 from src.chains.llm_agent import ChatBot
 
-DATA_PATH = "data/raw_data/SPOTIFY_REVIEW.csv"  
+DATA_PATH = "data/processed/SPOTIFY_REVIEWS.csv"  
 FAISS_PATH = "vectorstore/"
 EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 RAG_K_THRESHOLD = 10
@@ -36,12 +36,15 @@ class RagRetriever():
     def retrieve_by_time_and_rating(self, question: str, rating_threshold: float = None, time_window_days: int = None):
         """
         Retrieves documents by filtering based on satisfaction (review_rating) and time (review_timestamp).
-        Filters by the last `time_window_days` and/or ratings below `rating_threshold`.
+        Filters by the last `time_window_days` and/or ratings below `rating_threshold`, based on max review_timestamp in the dataset.
         """
         docs_score = self.vectorstore.similarity_search_with_score(question, RAG_K_THRESHOLD)
         filtered_docs = []
         
+        # Find the maximum timestamp from the dataset (to simulate the current date)
         max_timestamp = pd.to_datetime(self.df['review_timestamp']).max()
+
+        # Calculate time threshold relative to the max_timestamp
         time_threshold = max_timestamp - timedelta(days=time_window_days) if time_window_days else None
 
         for doc, score in docs_score:
@@ -50,8 +53,9 @@ class RagRetriever():
             review_rating = float(review["review_rating"])
             review_timestamp = pd.to_datetime(review["review_timestamp"])
 
+            # Filter based on rating and timestamp relative to max_timestamp
             if (rating_threshold is not None and review_rating <= rating_threshold) or \
-               (time_window_days is not None and review_timestamp >= time_threshold):
+            (time_window_days is not None and review_timestamp >= time_threshold):
                 filtered_docs.append((doc, score))
 
         return filtered_docs
@@ -73,7 +77,7 @@ class SelfQueryRetriever(RagRetriever):
         
         # If the query type is related to trends, retrieve data from the last 3 months based on max date in the data
         if query_type == "trends":
-            three_months_in_days = 90  # Last 3 months in days
+            three_months_in_days = 500  # Last 3 months in days
             print("Retrieving using time and rating filter for trends...")
             docs = self.retrieve_by_time_and_rating(question, time_window_days=three_months_in_days)
         else:
@@ -82,3 +86,4 @@ class SelfQueryRetriever(RagRetriever):
         response = self.chatbot.handle_query(question, docs, query_type)
         
         return response
+
